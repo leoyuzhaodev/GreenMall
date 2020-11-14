1125a67cb369c744e198797afa7aff3e2785c1b8
leo <leo_yuzhao_dev@aliyun.com>
2020-11-14 10:37:25 +0800

    商品分类前台数据展示BO。

---
+++ greenmall-entity/src/main/java/com/yzf/greenmall/bo/CategoryTreeBo.java
@@ -0,0 +1,26 @@
+package com.yzf.greenmall.bo;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @description:商品分类树
+ * @author:leo_yuzhao
+ * @date:2020/11/13
+ */
+@Data
+public class CategoryTreeBo {
+    private String title;
+    private Long id;
+    private List<CategoryTreeBo> children;
+
+    public CategoryTreeBo() {
+    }
+
+    public CategoryTreeBo(String title, Long id) {
+        this.title = title;
+        this.id = id;
+    }
+
+}
